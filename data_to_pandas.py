0dd2e617d4863ead4b4c5e468b9ee84130911f2e
Ben Webb <bjwebb67@googlemail.com>
2015-05-13 16:29:47 +0100

    Add an ipython notebook showing common fields between different datasets

--- data_to_pandas.py
+++ data_to_pandas.py
@@ -1,112 +1,45 @@
-
-# coding: utf-8
-
-# In[321]:
-
 import pandas as pd
+from collections import OrderedDict
 
+data_frames = OrderedDict()
 
-# In[322]:
+data_frames['indonesia-eiti-projects'] = pd.read_excel('data/indonesia/1-indonesia-eiti-projects.xlsx', sheetname='EITI project level data sample')
+data_frames['indonesia-mining-licenses'] = pd.read_excel('data/indonesia/1-indonesia-eiti-projects.xlsx', sheetname='Mining license sample')
+data_frames['indonesia-mining-licenses']['country'] = 'Indonesia' # Check that this is correct (ie. that this sheet is all Indonesia)
 
-df1_1 = pd.read_excel('data/indonesia/1-indonesia-eiti-projects.xlsx', sheetname='EITI project level data sample')
-df1_1['source'] = 'indonesia-eiti-projects'
-df1_2 = pd.read_excel('data/indonesia/1-indonesia-eiti-projects.xlsx', sheetname='Mining license sample')
-df1_2['source'] = 'indonesia-mining-licenses'
-df1_2['country'] = 'Indonesia' # Check that this is correct (ie. that this sheet is all Indonesia)
+data_frames['usgs'] = pd.read_csv('data/minfac.csv', encoding='latin-1')
 
+data_frames['indonesia-openoil-concessions'] = pd.read_csv('data/indonesia/3-openoil-concessions-indonesia.csv')
+data_frames['indonesia-openoil-concessions']['country'] = 'Indonesia'
 
-# In[323]:
+data_frames['indonesia-openoil-contracts'] = pd.read_csv('data/indonesia/4-openoil-contracts-indonesia.csv')
+data_frames['indonesia-openoil-contracts']['country'] = 'Indonesia'
 
-df2 = pd.read_csv('data/minfac.csv', encoding='latin-1')
-df2['source'] = 'usgs'
-df3 = pd.read_csv('data/indonesia/3-openoil-concessions-indonesia.csv')
-df3['source'] = 'indonesia-openoil-concessions'
-df3['country'] = 'Indonesia'
-df4 = pd.read_csv('data/indonesia/4-openoil-contracts-indonesia.csv')
-df4['source'] = 'indonesia-openoil-contracts'
-df4['country'] = 'Indonesia'
-
-
-# In[324]:
-
-for df in [df1_1, df1_2, df3, df4]:
-    df.columns = map(str.lower, df.columns)
-    df.columns = [str.lower(x).replace(' / ', '_').replace(' ', '_').replace('?', '').replace('(us$)', 'usd') for x in df.columns]
-#df.rename(columns={
-#        'Country': 'country',
-#    })
-df1_1 = df1_1.rename(columns={
+MAPPINGS = OrderedDict()
+MAPPINGS['indonesia-eiti-projects'] = {
         'commodity_type': 'commodity',
-    })
-df1_2 = df1_2.rename(columns={
+    }
+MAPPINGS['indonesia-mining-licenses'] = {
         'komoditas / commodity?': 'commodity',
         'tahun': 'year',
         'nama_perusahaan / company name': 'company_name'
-    })
-df2 = df2.rename(columns={
+    }
+MAPPINGS['usgs'] = {
         'op_comp': 'company_name',
         'capacity': 'production_volume',  # Not 100% sure these are the same, needs checking
         'units': 'production_unit',  # Not 100% sure these are the same, needs checking
-    })
-df3 = df3.rename(columns={
+    }
+MAPPINGS['indonesia-openoil-concessions'] = {
         'concessioncontractor': 'company_name',
-    })
-df4 = df4.rename(columns={
+    }
+MAPPINGS['indonesia-openoil-contracts'] = {
         'contractor': 'company_name',
-    })
+    }
 
+for key, df in data_frames.items():
+    df['source'] = key
 
-# In[325]:
+    df.columns = map(str.lower, df.columns)
+    df.columns = [str.lower(x).replace(' / ', '_').replace(' ', '_').replace('?', '').replace('(us$)', 'usd') for x in df.columns]
 
-combined_df = pd.concat([df1_1, df1_2, df2, df3, df4])
-
-
-# In[326]:
-
-fields = combined_df.groupby('source').count().transpose()
-fields
-
-
-# In[327]:
-
-sources_per_field = fields.apply(lambda x: x>0).sum(axis=1)
-sources_per_field.sort(ascending=False)
-sources_per_field
-
-
-# In[328]:
-
-df_idn = combined_df[combined_df['country'] == 'Indonesia']
-fields = combined_df.groupby('source').count().transpose()
-fields
-
-
-# In[365]:
-
-company_names_by_source = df_idn.groupby('company_name')['source'].nunique()
-company_names_by_source.sort(ascending=False)
-company_names_gt1_source = company_names_by_source[company_names_by_source > 1]
-company_names_gt1_source
-
-
-# In[366]:
-
-#for name, group in df_idn.groupby('company_name')['source']:
-#    if group.nunique() > 1:
-#        print(name)
-#        print(group.value_counts())
-
-
-# In[380]:
-
-sources = df_idn.source.unique()
-series = [ pd.Series(df_idn[df_idn['source'] == source].company_name.unique()) for source in sources ]
-df = pd.concat(series, axis=1)
-df.columns = df_idn.source.unique()
-df.count()
-
-
-# In[383]:
-
-df
-
+    data_frames[key] = df.rename(columns=MAPPINGS[key])
