38b623d6710586c185a985732542afe2489cd493
Ben Webb <bjwebb67@googlemail.com>
2015-05-13 16:50:05 +0100

    Update create_rdf to work with the new data_to_pandas

--- create_rdf.py
+++ create_rdf.py
@@ -1,11 +1,17 @@
-from data_to_pandas import combined_df
+import pandas as pd
+from data_to_pandas import data_frames
 from rdflib import Graph, URIRef, Literal
 
+
+combined_df = pd.concat(data_frames)
+
+
 g = Graph()
-for i, row in combined_df.iterrows():
-    subject = URIRef('http://localhost/s/'+row['source']+'/'+str(i))
+for line, row in combined_df.iterrows():
+    subject = URIRef('http://localhost/s/{}/{}'.format(*line))
     for k,v in row.dropna().items():
         g.add( (subject, URIRef('http://localhost/p/'+k), Literal(v) ) )
+
 
 g.serialize(destination='resource-projects.rdf')
 g.serialize(format='turtle', destination='resource-projects.turtle')
