6a3a62a0e60eeac9f5b6c61954d231036fd3d3cf
timgdavies <tim@timdavies.org.uk>
2015-06-30 11:31:14 +0100

    Updating statoil with year of payment

--- process/statoil/transform.py
+++ process/statoil/transform.py
@@ -5,6 +5,7 @@ from taglifter import TagLifter
 ## Note, we're currently using a customised version of the data, not the full data
 tl = TagLifter(ontology = "../../ontology/resource-projects-ontology.rdf",source = "sources/statoil-2014-project-overview-refined.csv",base="http://www.resourceprojects.org/",source_meta={})
 
+tl.limit_rows = 5
 tl.build_graph()
 
 print(tl.graph.serialize(format='turtle',destination="../../data/statoil-2014-payments.ttl"))
