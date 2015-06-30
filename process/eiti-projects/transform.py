67ae8d249c0c721073246962a094d843fb06a061
Ben Webb <bjwebb67@googlemail.com>
2015-06-30 15:03:20 +0100

    resourceprojects.org should not have a www

--- process/eiti-projects/transform.py
+++ process/eiti-projects/transform.py
@@ -3,7 +3,7 @@ sys.path.insert(1, os.path.join(sys.path[0], '../../modules/'))
 from taglifter import TagLifter
 
 ## Note, we're currently using a customised version of the data, not the full data
-tl = TagLifter(ontology = "../../ontology/resource-projects-ontology.rdf",source = "sources/eiti-project-level.csv",base="http://www.resourceprojects.org/",
+tl = TagLifter(ontology = "../../ontology/resource-projects-ontology.rdf",source = "sources/eiti-project-level.csv",base="http://resourceprojects.org/",
             source_meta={"author":"Tim Davies","Source_Type":"official","Converted":"Today"})
 
 tl.limit_rows = 623
