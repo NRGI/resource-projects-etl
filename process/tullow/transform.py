770e13179c653b3a26578daa284fa6c9a5381e72
timgdavies <tim@timdavies.org.uk>
2015-06-30 12:10:43 +0100

    Adding Tullow 2014 Payments

---
+++ process/tullow/transform.py
@@ -0,0 +1,12 @@
+import sys, os
+sys.path.insert(1, os.path.join(sys.path[0], '../../modules/'))
+from taglifter import TagLifter
+
+## Note, we're currently using a customised version of the data, not the full data
+tl = TagLifter(ontology = "../../ontology/resource-projects-ontology.rdf",source = "sources/2014-tullow-cr-report-refined.csv",base="http://www.resourceprojects.org/",
+            source_meta={})
+
+tl.limit_rows = 5
+tl.build_graph()
+
+print(tl.graph.serialize(format='turtle',destination="../../data/tullow-2014-payments.ttl")) 
