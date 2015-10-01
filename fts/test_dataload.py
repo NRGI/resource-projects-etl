fbc9d77a1542f18b4155da2f5f97777e670d5fcf
Ben Webb <ben.webb@opendataservices.coop>
2015-10-01 14:02:14 +0100

    [#101] Add humanize tests for conversion and pushing to live/staging

--- fts/test_dataload.py
+++ fts/test_dataload.py
@@ -5,7 +5,7 @@ import time
 
 
 server_url = os.environ['SERVER_URL']
-
+prefix = '/'
 
 @pytest.fixture(scope="module")
 def browser(request):
@@ -15,57 +15,37 @@ def browser(request):
     return browser
 
 
-@pytest.mark.parametrize(('prefix', 'source_filename', 'expected_text', 'conversion_successful'), [
-    ('/', 'tenders_releases_2_releases.json', 'Fetch now', True),
-    ])
-def test_humanize_naturaltime(browser, source_filename, prefix, expected_text, conversion_successful):
+def test_humanize_naturaltime(browser):
     '''
       This test loads a file from a url into the resourceprojects interface.
       You should then be taken to a /dataload/x page
-      Click the e.g. Fetch Now button
-      Check to see that the fetch now message is filtered by the naturaltime filter
+      Click the each of the 'X now' buttons in turn
+      Check to see that the status message is filtered by the naturaltime filter
     '''
 
-    source_url = 'https://raw.githubusercontent.com/NRGI/resource-projects-etl/6a3a62a0e60eeac9f5b6c61954d231036fd3d3cf/process/statoil/sources/statoil-2014-project-overview-refined.csv'
+    source_url = 'https://raw.githubusercontent.com/NRGI/resource-projects-etl/ee55c2956d23ebfc7a71cb1994a149d966a3c2a7/fts/fixtures/statoil-4-rows.csv'
 
     browser.get(server_url + prefix)
     browser.find_element_by_partial_link_text('Link').click()
     time.sleep(0.5)
     browser.find_element_by_id('id_source_url').send_keys(source_url)
     browser.find_element_by_css_selector("#fetchURL > div.form-group > button.btn.btn-primary").click()
-    body_text = browser.find_element_by_tag_name('body').text
-    assert expected_text in body_text
-    
-    # We should still be in the correct app
-    if prefix == '/':
-        assert 'ResourceProjects DataLoad Dashboard' in browser.find_element_by_tag_name('body').text
-    #Click the Fetch now button
-    browser.find_element_by_css_selector("button.btn.btn-default.fetch").click()
-    assert 'now' in browser.find_element_by_tag_name('body').text
-    #Click the convert button
-    browser.find_element_by_css_selector("button.btn.btn-default.convert").click()
-    assert 'now' in browser.find_element_by_tag_name('body').text
 
-    #Click the Push to Staging button
-    browser.find_element_by_css_selector("button.btn.btn-default.staging").click()
-    assert 'now' in browser.find_element_by_tag_name('body').text
+    dataset_url = browser.current_url
+    dataload_url = server_url + prefix + 'dataload/'
 
-    #Click the Remove from Staging button
-    browser.find_element_by_css_selector("button.btn.btn-default.rm_staging").click()
-    assert 'now' in browser.find_element_by_tag_name('body').text
+    # Click each of the buttons
 
-    #Click the Push to Live button
-    browser.find_element_by_css_selector("button.btn.btn-default.live").click()
-    assert 'now' in browser.find_element_by_tag_name('body').text
+    for process_id in ['fetch', 'convert', 'staging', 'live']:
+        browser.get(dataset_url)
+        browser.find_element_by_css_selector("button.btn.btn-default.{}".format(process_id)).click()
+        assert 'now' in browser.find_element_by_tag_name('body').text
+        browser.get(dataload_url)
+        assert 'now' in browser.find_element_by_tag_name('body').text or \
+            'second ago' in browser.find_element_by_tag_name('body').text or \
+            'seconds ago' in browser.find_element_by_tag_name('body').text
 
-    #Click the Remove from Live button
-    browser.find_element_by_css_selector("button.btn.btn-default.rm_staging").click()
-    assert 'now' in browser.find_element_by_tag_name('body').text
 
-    browser.get(server_url + prefix + 'dataload/')
-    assert 'now' in browser.find_element_by_tag_name('body').text or \
-        'second ago' in browser.find_element_by_tag_name('body').text or \
-        'seconds ago' in browser.find_element_by_tag_name('body').text
 
     
 
