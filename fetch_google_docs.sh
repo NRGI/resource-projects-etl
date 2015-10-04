97408ceef9392579827409e2b1700252c699daa1
timgdavies <tim@timdavies.org.uk>
2015-10-04 13:41:28 +0100

    Testing with large number of files

--- fetch_google_docs.sh
+++ fetch_google_docs.sh
@@ -1,28 +1,25 @@
 cd process/google-docs-reader/
 
-#echo "EITI"
-
-#python transform-from-gdocs.py https://docs.google.com/spreadsheets/d/1zEqqjv3ifCDQKjqSCpzqoBJ6hAhl_OO9u0eXfryIGUg/edit?usp=drive_web eiti
-
-#echo "StatOil"
-
-#python transform-from-gdocs.py https://docs.google.com/spreadsheets/d/1ZOPwbn98xlccJ01uDXIRNxPxQAnWJ99bQTtpelSuyEY/edit?usp=drive_web statoil
-
-#echo "Tullow"
-
-#python transform-from-gdocs.py https://docs.google.com/spreadsheets/d/11rpwhBfAPMHxnnHvqdp9RwZNOhbTvsLXxnHZtW_tS3M/edit?usp=drive_web tullow
-
-#echo "Kosmos"
-
-#python transform-from-gdocs.py https://docs.google.com/spreadsheets/d/18nucm2qWFjrRilpnaKFC0A8U-myZxdJN8dSYU2r_ZzY/edit?usp=drive_web kosmos
-
-
-#echo "Kosmos"
-
-#python transform-from-gdocs.py https://docs.google.com/spreadsheets/d/18nucm2qWFjrRilpnaKFC0A8U-myZxdJN8dSYU2r_ZzY/edit?usp=drive_web kosmos
-
 echo "Angola"
 python transform-from-gdocs.py https://docs.google.com/spreadsheets/d/1j9IDzCxCd2a70viGa8UejqWfxJDrUS87u7ROl8p8Hgc/edit#gid=1333644202 angola
 
 echo "Tullow"
 python transform-from-gdocs.py https://docs.google.com/spreadsheets/d/1FFFmO6y62NCM8eJKGs_QWnEImwzU0RJ26EYwRh8qCpk/edit#gid=279124923 tullow
+
+echo "Kosmos"
+python transform-from-gdocs.py https://docs.google.com/spreadsheets/d/1PqOOcgbshEVo54oMJenW8HBWekAAOlnm2XKW7NE4MQA/edit#gid=1333644202 kosmos
+
+echo "Statoil"
+python transform-from-gdocs.py https://docs.google.com/spreadsheets/d/17FDwVbDCReeWxufE2Gab1yw1PqJpQIt6pASM92d7sl4/edit#gid=204566568 statoil
+
+echo "Norway"
+python transform-from-gdocs.py https://docs.google.com/spreadsheets/d/1VAIeZus4yscc5J2hSS1OBbD_9TCvu7YukAExupFwZp4/edit norwaytheil
+
+echo "MLee"
+python transform-from-gdocs.py https://docs.google.com/spreadsheets/d/1KDhQpIXLcy4uI8FJ17W9r7NPQIiQBdzjSzSCX7K9L7w/edit#gid=1333644202 mlee
+
+echo "EITI"
+python transform-from-gdocs.py https://docs.google.com/spreadsheets/d/1_A02orMgpdlhdj5FheurrnY4ieO0HdTQNJ33pBjuYpY/edit#gid=1600730055 eiti
+
+echo "Mali"
+python transform-from-gdocs.py https://docs.google.com/spreadsheets/d/1bdDzInl_sn6TIbAuSF49b3flzbdJHvuSOdskmByaInE/edit#gid=141760938 mali
