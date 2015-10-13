72ae3bf524115e8e463e42b1b99e22053d27f075
Ben Webb <ben.webb@opendataservices.coop>
2015-10-13 12:59:44 +0100

    space

--- virtuoso_setup.sql
+++ virtuoso_setup.sql
@@ -1,4 +1,4 @@
 DB.DBA.RDF_GRAPH_GROUP_CREATE('http://staging.resourceprojects.org/data/', 1);
 DB.DBA.RDF_GRAPH_GROUP_CREATE('http://live.resourceprojects.org/data/', 1);
- insert into DB.DBA.SYS_SPARQL_HOST (SH_HOST, SH_GRAPH_URI) values ('%live%', 'http://live.resourceprojects.org/data/');
+insert into DB.DBA.SYS_SPARQL_HOST (SH_HOST, SH_GRAPH_URI) values ('%live%', 'http://live.resourceprojects.org/data/');
 insert into DB.DBA.SYS_SPARQL_HOST (SH_HOST, SH_GRAPH_URI) values ('%staging%', 'http://staging.resourceprojects.org/data/');
